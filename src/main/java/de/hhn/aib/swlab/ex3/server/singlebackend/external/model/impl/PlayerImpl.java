0819bb061b59b6078263cf1e1e1b44f5d9987a10
Pinolpier <14354249+Pinolpier@users.noreply.github.com>
2020-01-04 18:47:09 +0100

    included Simon's fix for PlayerImpl. Will test now

--- src/main/java/de/hhn/aib/swlab/ex3/server/singlebackend/external/model/impl/PlayerImpl.java
+++ src/main/java/de/hhn/aib/swlab/ex3/server/singlebackend/external/model/impl/PlayerImpl.java
@@ -3,10 +3,26 @@ package de.hhn.aib.swlab.ex3.server.singlebackend.external.model.impl;
 import de.hhn.aib.swlab.ex3.server.singlebackend.external.model.Player;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class PlayerImpl implements Player {
     private String name;
     private int playerIndex;
     private String token;
     private String gameId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return
+                false;
+        PlayerImpl player = (PlayerImpl) o;
+        return name.equals(player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
