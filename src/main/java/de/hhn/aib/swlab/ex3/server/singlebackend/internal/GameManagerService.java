e3efa72d4dfd7bfc4f743c6f96eae61572ad5061
Pinolpier <14354249+Pinolpier@users.noreply.github.com>
2020-01-04 15:14:54 +0100

    trying to find out which value is null because after creating game and joining player to it a NullPointer exception kills the game.

--- src/main/java/de/hhn/aib/swlab/ex3/server/singlebackend/internal/GameManagerService.java
+++ src/main/java/de/hhn/aib/swlab/ex3/server/singlebackend/internal/GameManagerService.java
@@ -2,6 +2,7 @@ package de.hhn.aib.swlab.ex3.server.singlebackend.internal;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import com.sun.tools.sjavac.Log;
 import de.hhn.aib.swlab.ex3.server.singlebackend.external.model.Player;
 import de.hhn.aib.swlab.ex3.server.singlebackend.external.model.impl.MessageImpl;
 import de.hhn.aib.swlab.ex3.server.singlebackend.game.GameMessage;
@@ -184,6 +185,13 @@ public class GameManagerService {
     }
 
     public void passMessageToPlayer(String message, Player player) {
+        Log.info("passMessageToPlayer has been called. Message is null? " + (message == null) + " Player is null? " + (player == null));
+        if (message != null) {
+            Log.info("message is not null, message is: " + message);
+        }
+        if (player != null) {
+            Log.info("player is not null, player.getName is: " + player.getName());
+        }
         try {
             log.debug("Passing message {} to {}", message, player.getName());
             synchronized (this.playerToWebSocketSession.get(player)) {
